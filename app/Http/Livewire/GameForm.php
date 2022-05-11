2136361923135bc475cf8abf85dcb6b6c9b0d05d
GermanZun <german.zuniga@gmail.com>
2022-05-11 01:00:03 -0400

    refactor, bind to game model properties

--- app/Http/Livewire/GameForm.php
+++ app/Http/Livewire/GameForm.php
@@ -9,50 +9,35 @@ use Livewire\Component;
 class GameForm extends Component
 {
     public $statuses;
-    public $game_id;
-    public $name;
-    public $description;
-    public $url;
-    public $image_url;
-    public $status_id;
+    public Game $game;
+
+    protected $rules = [
+        'game.name' => 'required|min:3',
+        'game.description' => '',
+        'game.url' => 'required',
+        'game.image_url' => 'required',
+        'game.status_id' => ''
+    ];
 
     protected $listeners = ['fillForm' => 'edit'];
 
     public function mount()
     {
-        $this->setStatuses();
-    }
-
-    private function setStatuses()
-    {
+        $this->game = new Game();
+        $this->game->status_id = "1";
         $this->statuses = Status::all();
-        $this->status_id = "1";
     }
 
     public function edit(Game $game)
     {
-        $this->game_id = $game->id;
-        $this->name = $game->name;
-        $this->description = $game->description;
-        $this->url = $game->url;
-        $this->image_url = $game->image_url;
-        $this->status_id = $game->status_id;
+        $this->game = $game;
     }
 
     public function createEditGame()
     {
-        $validatedData = $this->validate([
-            'name' => 'required|min:3',
-            'description' => '',
-            'url' => 'required',
-            'image_url' => 'required',
-            'status_id' => '',
-        ]);
-
-        Game::updateOrCreate(['id' => $this->game_id], $validatedData);
-
-        $this->reset();
-        $this->setStatuses();
+        $this->validate();
+        $this->game->save();
+        $this->mount();
         $this->emit('refreshList');
     }
 
--- resources/views/livewire/game-form.blade.php
+++ resources/views/livewire/game-form.blade.php
@@ -13,41 +13,41 @@
                         <div class="grid grid-cols-6 gap-6">
                             <div class="col-span-6 sm:col-span-6">
                                 <label for="for_name" class="block text-sm font-medium text-gray-700">Nombre</label>
-                                <input wire:model.defer="name" type="text" name="name" id="for_name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                @error('name') <span class="text-red-500">{{ $message }}</span> @enderror
+                                <input wire:model.defer="game.name" type="text" name="name" id="for_name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                @error('game.name') <span class="text-red-500">{{ $message }}</span> @enderror
                             </div>
 
                             <div class="col-span-6 sm:col-span-6">
                                 <label for="for_description" class="block text-sm font-medium text-gray-700">Descripción</label>
-                                <input wire:model.defer="description" type="text" name="description" id="for_description" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <input wire:model.defer="game.description" type="text" name="description" id="for_description" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
 
-                                @error('description') <span class="text-red-500">{{ $message }}</span> @enderror
+                                @error('game.description') <span class="text-red-500">{{ $message }}</span> @enderror
                             </div>
 
 
                             <div class="col-span-6">
                                 <label for="for_url" class="block text-sm font-medium text-gray-700">Url</label>
-                                <input wire:model.defer="url" type="text" name="url" id="for_url" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <input wire:model.defer="game.url" type="text" name="url" id="for_url" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
 
-                                @error('url') <span class="text-red-500">{{ $message }}</span> @enderror
+                                @error('game.url') <span class="text-red-500">{{ $message }}</span> @enderror
                             </div>
 
                             <div class="col-span-6 sm:col-span-6 ">
                                 <label for="for_image_url" class="block text-sm font-medium text-gray-700">Url Imagen</label>
-                                <input wire:model.defer="image_url" type="text" name="image_url" id="for_image_url" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <input wire:model.defer="game.image_url" type="text" name="image_url" id="for_image_url" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
 
-                                @error('imageUrl') <span class="text-red-500">{{ $message }}</span> @enderror
+                                @error('game.image_url') <span class="text-red-500">{{ $message }}</span> @enderror
                             </div>
 
                             <div class="col-span-6 sm:col-span-3">
                                 <label for="for_status" class="block text-sm font-medium text-gray-700">Estado</label>
-                                <select wire:model.defer="status_id" id="for_status" name="status_id" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <select wire:model.defer="game.status_id" id="for_status" name="status_id" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                     @foreach($statuses as $status)
                                         <option value={{$status->id}}>{{$status->name}}</option>
                                     @endforeach
                                 </select>
 
-                                @error('statusId') <span class="text-red-500">{{ $message }}</span> @enderror
+                                @error('game.status_id') <span class="text-red-500">{{ $message }}</span> @enderror
                             </div>
                         </div>
                     </div>
