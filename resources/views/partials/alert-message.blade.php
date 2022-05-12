82395ac92fa68f5a3cedd6be3e7563f733d6500d
GermanZun <german.zuniga@gmail.com>
2022-05-11 21:26:38 -0400

    add close button alert

--- resources/views/partials/alert-message.blade.php
+++ resources/views/partials/alert-message.blade.php
@@ -1,8 +1,19 @@
 @if(session()->has('message'))
-    <div class="bg-indigo-100 rounded-lg py-5 px-6 mb-4 text-base text-indigo-700 mb-3" role="alert">
-        {!! session()->get('message') !!}
-        <button type="button"
-                class="btn-close box-content w-4 h-4 p-1 ml-auto text-yellow-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-yellow-900 hover:opacity-75 hover:no-underline"
-                data-bs-dismiss="alert" aria-label="Close"></button>
+
+    <div class="flex justify-between text-indigo-700 shadow-inner rounded py-5 px-6 mb-4 bg-indigo-100">
+        <p class="self-center">
+            <strong>Guardado </strong> {!! session()->get('message') !!}
+        </p>
+        <strong class="text-xl align-center cursor-pointer alert-del">&times;</strong>
     </div>
+
+    <script>
+        const alert_del = document.querySelectorAll('.alert-del');
+        alert_del.forEach((x) =>
+            x.addEventListener('click', function () {
+                x.parentElement.classList.add('hidden');
+            })
+        );
+    </script>
+
 @endif
